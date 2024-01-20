19103bce24f247acdaaf2756ac239d9ea4e8a60d
Rinx-1 <1399937361@qq.com>
2024-01-21 03:31:59 +0800

    project init

---
+++ React-1/project/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+// import 'antd/dist/antd.css';
+import './App.css';
+
+const App: React.FC = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App;
